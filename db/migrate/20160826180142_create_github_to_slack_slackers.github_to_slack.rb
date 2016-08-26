1f66b4f3f24dda365f3c9e23df2de23d572cc370
Jake Waletzko <jake.waletzko@sportngin.com>
2016-08-26 13:02:01 -0500

    new migration

---
+++ db/migrate/20160826180142_create_github_to_slack_slackers.github_to_slack.rb
@@ -0,0 +1,12 @@
+# This migration comes from github_to_slack (originally 20160826175603)
+class CreateGithubToSlackSlackers < ActiveRecord::Migration
+  def change
+    create_table :github_to_slack_slackers do |t|
+      t.string :webhook_url
+      t.string :channel_name
+      t.string :username
+
+      t.timestamps null: false
+    end
+  end
+end
